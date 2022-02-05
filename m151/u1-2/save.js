25e47511ee0be48cfbc9404350dd2006b54b0c13
Daniel Santos Sousa <Daniel_SantosSousa@sluz.ch>
2022-02-05 17:48:30 +0100

    m151 u1-2

---
+++ m151/u1-2/save.js
@@ -0,0 +1,14 @@
+export function saveAddress(addresses, address) {
+    if (address.id) {
+        const index = addresses.findIndex((adr) => {
+            return adr.id === +address.id;
+        });
+        address.id = +address.id;
+        addresses[index] = address;
+    } else {
+        const nextId = Math.max(...addresses.map((address) => address.id)) + 1;
+        address.id = nextId;
+        addresses.push(address);
+    }
+    return addresses;
+}
